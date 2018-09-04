09d705a3e59d93e558d77c32d9d9d434bb9a2918
Luca <musso.luca89@gmail.com>
2018-09-04 16:42:51 +0200

    settati controlli, manca codice di inviare dati al DB dopo aver inserito i valori

--- Assets/Scripts/Eviso/EvisoAutoInsert.cs
+++ Assets/Scripts/Eviso/EvisoAutoInsert.cs
@@ -14,30 +14,33 @@ public class EvisoAutoInsert : MonoBehaviour {
 	public Image f3ButtonImage;
 	public Color startColor;
 	public Color stopColor;
+	public Text sendResult; 
 	string supportInputF1;
 	bool checkF1;
 	bool checkF2;
 	bool checkF3;
 	bool checkToSend;
+	bool startDissolve;
+	public float t;
 	// Use this for initialization
 	void Start () {
-		date.text = "" + System.DateTime.Now.ToString ("dd/MM/yyyy");
-		f1Input.characterLimit = 7;
+		date.text = "" + System.DateTime.Now.ToString ("dd/MM/yyyy"); // visualizzo la data odierna come controlo
+		f1Input.characterLimit = 7; // lunghezza massimo di inserimento valori energia
 		f2Input.characterLimit = 7;
 		f3Input.characterLimit = 7;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-//		Debug.Log ("F1: " + f1Input.text + " F2: " + f2Input.text + " F3: " + f3Input.text);
-//		Debug.Log ("F1: " + f1Input.text.Length + " F2: " + f2Input.text.Length + " F3: " + f3Input.text.Length);
-//		if(f1Input.text.Length == 0){
-//			CheckStringNumber(f1Input.text);
-//		}
+		sendResult.color = new Vector4 (0, 0, 0, 0); // setto invisibile la scritta dell'OK dei dati i nviati al DB
+		sendResult.text = "SEND OK";
 	}
 
+	void FixedUpdate(){
+		// facci opartire la dissolvenza della scritta dell'ok dell'invio dei dati al DB
+		if (startDissolve) {
+			DissolveText (sendResult);
+		}
+	}
 	// check per controllare se hanno inserito i valori, se non inseriti devo farli inserire a loro bloccando e colorando di rossi i pulsanti 
 	public bool CheckNumberToSend(InputField f1InputCheck,InputField f2InputCheck,InputField f3Inputcheck){
+		// controllo sui 3 inserimento in F1,F2,F3
 		if (f1InputCheck.text.Length == 0) {
 			checkF1 = true;
 			f1ButtonImage.color = stopColor;
@@ -59,6 +62,7 @@ public class EvisoAutoInsert : MonoBehaviour {
 			checkF3 = false;
 			f3ButtonImage.color = startColor;
 		}
+		// se tutti e 3 sono abilitati allora posso inviare i dati
 		if (checkF1 || checkF2 || checkF3) {
 			return false;
 		} else {
@@ -69,9 +73,25 @@ public class EvisoAutoInsert : MonoBehaviour {
 	// quando si preme si controlla se tutti i campi inseriti e in caso no nti fa mandare il messaggio al server
 	public void SendNumbers(){
 		if (CheckNumberToSend (f1Input, f2Input, f3Input)) {
+			// se supera il controllo invio i valori al DB e risetto i pulsanti come predefiniti, faccio anche partire la dissolvenza
 			Debug.Log ("MANDARE!!");
+			f1Input.text = "";
+			f1ButtonImage.color = startColor;
+			f2Input.text = "";
+			f2ButtonImage.color = startColor;
+			f3Input.text = "";
+			f3ButtonImage.color = startColor;
+			t = 0;
+			sendResult.color = new Vector4 (0, 0, 0, 1);
+			startDissolve = true;
 		} else {
 			Debug.Log ("NO!!!");
 		}
+	}
+
+	// ndissolve l'ok dell'invio dei dati
+	public void DissolveText(Text sendResultToDissolve){
+		t += 0.2f * Time.deltaTime;
+		sendResultToDissolve.color = new Vector4 (0, 0, 0, Mathf.Lerp(1,0,t));
 	}
 }
