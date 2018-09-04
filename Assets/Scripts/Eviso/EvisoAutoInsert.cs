c851f102e46ce4bbdcbaec3b648b641857059906
Luca <musso.luca89@gmail.com>
2018-09-04 14:47:51 +0200

    Modificati pulsanti e inizio controlli su inserimento dei valori da inserire

--- Assets/Scripts/Eviso/EvisoAutoInsert.cs
+++ Assets/Scripts/Eviso/EvisoAutoInsert.cs
@@ -9,7 +9,16 @@ public class EvisoAutoInsert : MonoBehaviour {
 	public InputField f1Input;
 	public InputField f2Input;
 	public InputField f3Input;
+	public Image f1ButtonImage;
+	public Image f2ButtonImage;
+	public Image f3ButtonImage;
+	public Color startColor;
+	public Color stopColor;
 	string supportInputF1;
+	bool checkF1;
+	bool checkF2;
+	bool checkF3;
+	bool checkToSend;
 	// Use this for initialization
 	void Start () {
 		date.text = "" + System.DateTime.Now.ToString ("dd/MM/yyyy");
@@ -20,18 +29,49 @@ public class EvisoAutoInsert : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("F1: " + f1Input.text + " F2: " + f2Input.text + " F3: " + f3Input.text);
-		if(f1Input.text.Length > 6){
+//		Debug.Log ("F1: " + f1Input.text + " F2: " + f2Input.text + " F3: " + f3Input.text);
+//		Debug.Log ("F1: " + f1Input.text.Length + " F2: " + f2Input.text.Length + " F3: " + f3Input.text.Length);
+//		if(f1Input.text.Length == 0){
 //			CheckStringNumber(f1Input.text);
+//		}
+	}
+
+	// check per controllare se hanno inserito i valori, se non inseriti devo farli inserire a loro bloccando e colorando di rossi i pulsanti 
+	public bool CheckNumberToSend(InputField f1InputCheck,InputField f2InputCheck,InputField f3Inputcheck){
+		if (f1InputCheck.text.Length == 0) {
+			checkF1 = true;
+			f1ButtonImage.color = stopColor;
+		} else {
+			checkF1 = false;
+			f1ButtonImage.color = startColor;
+		}
+		if (f2InputCheck.text.Length == 0) {
+			checkF2 = true;
+			f2ButtonImage.color = stopColor;
+		} else {
+			checkF2 = false;
+			f2ButtonImage.color = startColor;
+		}
+		if (f3Inputcheck.text.Length == 0) {
+			checkF3 = true;
+			f3ButtonImage.color = stopColor;
+		} else {
+			checkF3 = false;
+			f3ButtonImage.color = startColor;
+		}
+		if (checkF1 || checkF2 || checkF3) {
+			return false;
+		} else {
+			return true;
 		}
 	}
 
-	// da fare check!!! non funziona
-//	public void CheckStringNumber(string stringToCheck){
-//		for (int i = 0; i < stringToCheck.Length; i++) {
-//			char value = stringToCheck [i];
-//			int number = (int)stringToCheck [i];
-//			Debug.Log ("NUM: " + number);
-//		}
-//	}
+	// quando si preme si controlla se tutti i campi inseriti e in caso no nti fa mandare il messaggio al server
+	public void SendNumbers(){
+		if (CheckNumberToSend (f1Input, f2Input, f3Input)) {
+			Debug.Log ("MANDARE!!");
+		} else {
+			Debug.Log ("NO!!!");
+		}
+	}
 }
