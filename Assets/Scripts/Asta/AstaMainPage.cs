8cbc7d350dc89a5306f3c1f55e1ae09240beab23
Luca <musso.luca89@gmail.com>
2019-01-21 13:32:40 +0100

    Creata pagina principale con navigazione

---
+++ Assets/Scripts/Asta/AstaMainPage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstaMainPage : MonoBehaviour {
+
+	public static AstaMainPage instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenLoginPage(){
+		Debug.Log("Entrato ");
+		AstaPageManager.instance.CustomClick ("EvisoChoice");
+	}
+
+	public void OpenMyPlayersPage(){
+		Debug.Log("Entrato My Players");
+		AstaPageManager.instance.AstaLoginMyPlayers ();
+	}
+
+	public void OpenMarketPage(){
+		Debug.Log("Entrato Market");
+		AstaPageManager.instance.AstaLoginMarket ();
+	}
+}
---
+++ Assets/Scripts/Asta/AstaPageManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AstaPageManager : MonoBehaviour {
+
+	public static AstaPageManager instance;
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// controllo sul tasto di ritorno back
+		if (Input.GetKey (KeyCode.Escape)) { 
+			var actualScene = SceneManager.GetActiveScene ();
+			switch (actualScene.name) { // in base a ogni scenaq torno indietro a quella che mi serve
+			case "AstaMain":
+				Debug.Log ("Sono nel Main non posso tornarte indietro");
+				break;
+			case "AstaMarket":
+				SceneManager.LoadScene("AstaMain");
+				break;
+			case "AstaMyPlayers":
+				SceneManager.LoadScene("AstaMain");
+				break;
+			}
+		}
+	}
+
+
+	public void CustomClick(string pageName)
+	{
+		SceneManager.LoadScene(pageName);
+	}
+
+	public void AstaLoginMarket()
+	{
+		SceneManager.LoadScene("AstaMarket");
+	}
+
+	public void AstaLoginMyPlayers()
+	{
+		SceneManager.LoadScene("AstaMyPlayers");
+	}
+}
