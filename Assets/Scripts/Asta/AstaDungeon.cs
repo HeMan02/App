1ff2748d5cc79f5552dd4dd51f5419c9f54d4e26
HeMan02 <musso.luca89@gmail.com>
2020-01-31 17:20:59 +0100

    Generazione lista characters Dungeon

--- Assets/Scripts/Asta/AstaDungeon.cs
+++ Assets/Scripts/Asta/AstaDungeon.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class AstaDungeon : MonoBehaviour
 {
     public GameObject scrollContentPAretDungeon;
+    public GameObject scrollContentPAretDungeonCharacters;
     // Start is called before the first frame update
     void Start()
     {
         GenerateDungeon();
+        GenerateDungeonCharacters();
     }
 
     // Update is called once per frame
@@ -25,15 +27,30 @@ public class AstaDungeon : MonoBehaviour
      public void GenerateDungeon()
     {
         List<AstaPageManager.Dungeon> listDungeon = AstaPageManager.Instance.listDungeon;
-        Debug.Log("Count" + listDungeon.Count);
         for (int i = 0; i < listDungeon.Count; i++)
         {
-             Debug.Log("Entrato");
              GameObject dungeonPrefab = Resources.Load("Missions") as GameObject;
              GameObject dungeon = Instantiate(dungeonPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
              dungeon.transform.SetParent(scrollContentPAretDungeon.transform, false);
              dungeon.name = "DUNGEON." + i.ToString(); 
              dungeon.GetComponent<AstaDungeonObj>().myId =  i;
+            //  dungeon.GetComponent<AstaDungeonObj>().time.text =  listDungeon[i].time;
+            //  dungeon.GetComponent<AstaDungeonObj>().description = listDungeon[i].description;
+            //  dungeon.GetComponent<AstaDungeonObj>().type = listDungeon[i].type;
+            //  dungeon.GetComponent<AstaDungeonObj>().coinsText = listDungeon[i].cashWin;
+        }
+    }
+
+      public void GenerateDungeonCharacters()
+    {
+        List<AstaPageManager.Character> listDungeonCharacter = AstaPageManager.Instance.listUserCharacters;
+        for (int i = 0; i < listDungeonCharacter.Count; i++)
+        {
+             GameObject dungeonCharacterSelectPrefab = Resources.Load("CharacterDungeonSelect") as GameObject;
+             GameObject dungeonCharacterSelect = Instantiate(dungeonCharacterSelectPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+             dungeonCharacterSelect.transform.SetParent(scrollContentPAretDungeonCharacters.transform, false);
+             dungeonCharacterSelect.name = "CHR." + i.ToString(); 
+             dungeonCharacterSelect.GetComponent<AstaDungeonCharacterSelectedObj>().myId =  i;
             //  dungeon.GetComponent<AstaDungeonObj>().time.text =  listDungeon[i].time;
             //  dungeon.GetComponent<AstaDungeonObj>().description = listDungeon[i].description;
             //  dungeon.GetComponent<AstaDungeonObj>().type = listDungeon[i].type;
---
+++ Assets/Scripts/Asta/AstaDungeonCharacterSelectedObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AstaDungeonCharacterSelectedObj : MonoBehaviour
+{
+    public int myId;
+    public Text name;
+    public List<AstaPageManager.Character> listDungeonCharacters;
+    // Start is called before the first frame update
+    void Start()
+    {
+        listDungeonCharacters = AstaPageManager.Instance.listUserCharacters;
+        name.text = "" + listDungeonCharacters[myId].name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
